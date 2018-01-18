c5d754fbce2020c2eba0a3dc081a8c4423d24825
Allie Del Giorno <adelgior@cs.cmu.edu>
2018-01-18 15:50:42 -0500

    trying to fix requires_grad breaks for Nonetype

--- examples/voc/train_fcn8s_atonce_instance_as_semantic.py
+++ examples/voc/train_fcn8s_atonce_instance_as_semantic.py
@@ -80,8 +80,10 @@ def main():
 
     optim = torch.optim.SGD(
         [
-            {'params': filter(lambda p: p.requires_grad, get_parameters(model, bias=False))},
-            {'params': filter(lambda p: p.requires_grad, get_parameters(model, bias=True)),
+            {'params': filter(lambda p: True if p is None else p.requires_grad, get_parameters(
+                model, bias=False))},
+            {'params': filter(lambda p: True if p is None else p.requires_grad, get_parameters(
+                model, bias=True)),
              'lr': cfg['lr'] * 2, 'weight_decay': 0},
         ],
         lr=cfg['lr'],

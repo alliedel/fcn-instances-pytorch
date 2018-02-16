f5f0579af75a31883c023e1fb4e994c28a02f753
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2018-02-16 12:42:19 -0500

    moved generic conv definitions to model_utils.py so other models can use them

--- torchfcn/models/model_utils.py
+++ torchfcn/models/model_utils.py
@@ -1,4 +1,61 @@
 import torchfcn
+from torch import nn
+
+
+# Choose non-linearities
+def get_nonlinearity(nonlinearity):
+    if nonlinearity == 'prelu':
+        return nn.PReLU()
+    elif nonlinearity == 'relu':
+        return nn.ReLU(inplace=True)
+    elif nonlinearity == 'tanh':
+        return nn.Tanh()
+    elif nonlinearity == 'sigmoid':
+        return nn.Sigmoid()
+    elif nonlinearity == 'elu':
+        return nn.ELU(inplace=True)
+    elif nonlinearity == 'selu':
+        return nn.SELU()
+    else:
+        assert False, "Unknown non-linearity: {}".format(nonlinearity)
+
+
+### Basic Conv + Pool + BN + Non-linearity structure
+class BasicConv2D(nn.Module):
+    def __init__(self, in_channels, out_channels, use_pool=False, use_bn=True, nonlinearity='prelu', **kwargs):
+        super(BasicConv2D, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, **kwargs)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2) if use_pool else None
+        self.bn = nn.BatchNorm2d(out_channels, eps=0.001) if use_bn else None
+        self.nonlin = get_nonlinearity(nonlinearity)
+
+    # Convolution -> Pool -> BN -> Non-linearity
+    def forward(self, x):
+        x = self.conv(x)
+        if self.pool:
+            x = self.pool(x)
+        if self.bn:
+            x = self.bn(x)
+        return self.nonlin(x)
+
+
+### Basic Deconv + (Optional Skip-Add) + BN + Non-linearity structure
+class BasicDeconv2D(nn.Module):
+    def __init__(self, in_channels, out_channels, use_bn=True, nonlinearity='prelu', **kwargs):
+        super(BasicDeconv2D, self).__init__()
+        self.deconv = nn.ConvTranspose2d(in_channels, out_channels, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels, eps=0.001) if use_bn else None
+        self.nonlin = get_nonlinearity(nonlinearity)
+
+    # BN -> Non-linearity -> Deconvolution -> (Optional Skip-Add)
+    def forward(self, x, y=None):
+        if y is not None:
+            x = self.deconv(x) + y  # Skip-Add the extra input
+        else:
+            x = self.deconv(x)
+        if self.bn:
+            x = self.bn(x)
+        return self.nonlin(x)
 
 
 def get_parameters(model, bias=False):

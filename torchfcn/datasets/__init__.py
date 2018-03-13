fae1a4d9e4a4d7fd1152b91987902c6d170bd666
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2018-03-13 16:47:14 -0400

    fixed import error

--- torchfcn/datasets/__init__.py
+++ torchfcn/datasets/__init__.py
@@ -3,6 +3,6 @@ from .voc import VOC2012ClassSeg  # NOQA
 from .instances import VOC2007ObjectSeg
 from .cityscapes import CityscapesMappedToInstances
 
-import voc
+from . import voc
 from . import pink_blobs
 from . import instances

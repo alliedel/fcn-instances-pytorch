e9711f5e6f53638be31068b08fc7015c0afe6b2f
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2018-04-04 16:05:19 -0400

    copied files over from initial pink_blobs branch

---
+++ examples/pink_blobs/train_fcn8s_pink_blobs.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import os.path as osp
+
+import torch
+import numpy as np
+
+import torchfcn
+from torchfcn.datasets import dataset_utils
+
+from examples.voc.script_utils import get_log_dir
+from examples.voc.script_utils import get_parameters
+
+from tensorboardX import SummaryWriter
+from torchfcn.datasets import pink_blobs
+
+import local_pyutils
+
+
+torch.manual_seed(0)
+torch.cuda.manual_seed_all(0)
+np.random.seed(0)
+
+
+# filename starts to exceed max; creating abbreviations so we can keep the config in the log
+# directory name.
+CONFIG_KEY_REPLACEMENTS_FOR_FILENAME = {'max_iteration': 'itr',
+                                        'weight_decay': 'decay',
+                                        'n_training_imgs': 'n_train',
+                                        'n_validation_imgs': 'n_val',
+                                        'recompute_optimal_loss': 'recompute_loss',
+                                        'size_average': 'size_avg'}
+
+default_configuration = dict(
+    max_iteration=10000,
+    lr=1.0e-5,
+    weight_decay=5e-6,
+    interval_validate=100,
+    n_max_per_class=3,
+    n_training_imgs=1000,
+    n_validation_imgs=50,
+    batch_size=1,
+    recompute_optimal_loss=False,
+    size_average=True,
+    val_on_train=True)
+
+configurations = {
+    # same configuration as original work
+    # https://github.com/shelhamer/fcn.berkeleyvision.org
+    0: dict(),
+    1: dict(
+        batch_size=10),
+    2: dict(
+        n_training_imgs=10),
+    3: dict(
+        n_training_imgs=100),
+    4: dict(
+        recompute_optimal_loss=True,
+        size_average=True
+    ),
+    5: dict(
+        recompute_optimal_loss=True,
+        size_average=False
+    ),
+    6: dict(
+        recompute_optimal_loss=False,
+        size_average=False
+    ),
+    7: dict(
+        batch_size=2),
+}
+
+here = osp.dirname(osp.abspath(__file__))
+logger = local_pyutils.get_logger()
+
+
+def create_config_copy(config_dict, config_key_replacements=CONFIG_KEY_REPLACEMENTS_FOR_FILENAME):
+    cfg_print = config_dict.copy()
+    for key, replacement_key in config_key_replacements.items():
+        cfg_print[replacement_key] = cfg_print.pop(key)
+    return cfg_print
+
+
+def main():
+    matching = True
+    assert_val_not_in_train = False
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--gpu', type=int, required=True)
+    parser.add_argument('-c', '--config', type=int, default=0,
+                        choices=configurations.keys())
+    parser.add_argument('--resume', help='Checkpoint path')
+    args = parser.parse_args()
+
+    gpu = args.gpu
+    cfg = default_configuration
+    cfg.update(configurations[args.config])
+    out = get_log_dir(osp.basename(__file__).replace(
+        '.py', ''), args.config, create_config_copy(cfg), parent_directory=osp.dirname(osp.abspath(
+        __file__)))
+
+    logger.info('logdir: {}'.format(out))
+    resume = args.resume
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu)
+    cuda = torch.cuda.is_available()
+
+    torch.manual_seed(1337)
+    if cuda:
+        torch.cuda.manual_seed(1337)
+
+    # 1. dataset
+    root = osp.expanduser('~/data/datasets')
+    kwargs = {'num_workers': 4, 'pin_memory': True} if cuda else {}
+    train_dataset = pink_blobs.BlobExampleGenerator(
+        transform=True, n_max_per_class=cfg['n_max_per_class'], max_index=cfg['n_training_imgs']
+                                                                          - 1)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=cfg['batch_size'],
+                                               shuffle=True)
+    if cfg['val_on_train']:
+        train_dataset_for_val = train_dataset.copy(modified_length=10)
+        train_loader_for_val = torch.utils.data.DataLoader(train_dataset_for_val, batch_size=1,
+                                                           shuffle=False)
+    else:
+        train_loader_for_val = None
+    val_dataset = pink_blobs.BlobExampleGenerator(transform=True,
+                                                  n_max_per_class=cfg['n_max_per_class'],
+                                                  max_index=cfg['n_validation_imgs'] - 1)
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=1, shuffle=False)
+
+    if assert_val_not_in_train:
+        logger.info('Checking whether validation images appear in the training set.')
+        dataset_utils.assert_validation_images_arent_in_training_set(train_loader, val_loader)
+        logger.info('Confirmed validation and training set are disjoint.')
+
+    # Make sure we can load an image
+    [img, lbl] = train_loader.dataset[0]
+
+    # 2. model
+    # n_max_per_class > 1 and map_to_semantic=False: Basically produces extra channels that
+    # should be '0'.  Not good if copying weights over from a pretrained semantic segmenter,
+    # but fine otherwise.
+    model = torchfcn.models.FCN8sInstance(
+        n_semantic_classes_with_background=len(train_loader.dataset.class_names),
+        n_max_per_class=cfg['n_max_per_class'],
+        map_to_semantic=False)
+
+    start_epoch = 0
+    start_iteration = 0
+    if resume:
+        checkpoint = torch.load(resume)
+        model.load_state_dict(checkpoint['model_state_dict'])
+        start_epoch = checkpoint['epoch']
+        start_iteration = checkpoint['iteration']
+    else:
+        vgg16 = torchfcn.models.VGG16(pretrained=True)
+        model.copy_params_from_vgg16(vgg16)
+    if cuda:
+        model = model.cuda()
+
+    # 3. optimizer
+    optim = torch.optim.Adam(model.parameters(), lr=cfg['lr'], weight_decay=cfg['weight_decay'])
+
+    if resume:
+        optim.load_state_dict(checkpoint['optim_state_dict'])
+
+    writer = SummaryWriter(log_dir=out)
+    trainer = torchfcn.Trainer(
+        cuda=cuda,
+        model=model,
+        optimizer=optim,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        out=out,
+        max_iter=cfg['max_iteration'],
+        interval_validate=cfg.get('interval_validate', len(train_loader)),
+        tensorboard_writer=writer,
+        matching_loss=matching,
+        recompute_loss_at_optimal_permutation=cfg['recompute_optimal_loss'],
+        size_average=cfg.get('size_average'),
+        train_loader_for_val=train_loader_for_val
+    )
+    trainer.epoch = start_epoch
+    trainer.iteration = start_iteration
+    trainer.train()
+
+
+if __name__ == '__main__':
+    main()
---
+++ torchfcn/datasets/pink_blobs.py
@@ -0,0 +1,288 @@
+import numpy as np
+import torch
+from torchfcn.datasets import dataset_utils
+from PIL import Image, ImageDraw
+
+PEPTO_BISMOL_PINK_RGB = (246, 143, 224)
+BLUE_RGB = (0, 0, 224)
+GREEN_RGB = (0, 225, 0)
+
+ALL_BLOB_CLASS_NAMES = np.array(['background', 'pink_square', 'blue_square'])
+
+
+class Defaults(object):
+    img_size = (281, 500)
+    blob_size = (40, 40)
+    clrs = [PEPTO_BISMOL_PINK_RGB, BLUE_RGB, GREEN_RGB]
+    blob_types = ['square', 'circle']
+    n_max_per_class = 3
+    n_instances_per_img = 2
+    return_torch_type = False
+    location_generation_type = 'random'  # 'moving'
+    velocity_r_c = [[0, 1], [0, -1]]
+    max_index = 100
+    mean_bgr = np.array([10.0, 10.0, 10.0])
+    transform = True
+
+
+class BlobExampleGenerator(object):
+    def __init__(self, img_size=Defaults.img_size, blob_size=Defaults.blob_size,
+                 clrs=Defaults.clrs,
+                 n_max_per_class=Defaults.n_max_per_class,
+                 n_instances_per_img=Defaults.n_instances_per_img,
+                 return_torch_type=Defaults.return_torch_type,
+                 max_index=Defaults.max_index, mean_bgr=Defaults.mean_bgr,
+                 transform=Defaults.transform, velocity_r_c=None,
+                 initial_rows=None, initial_cols=None, _im_a_copy=False):
+        self.img_size = img_size
+        assert len(self.img_size) == 2
+        self.blob_size = blob_size
+        self.clrs = clrs  # nested list: outer list -- one item per semantic class.  inner list
+        # -- list of colors for that semantic class.
+        self.n_instances_per_img = n_instances_per_img
+        self.n_max_per_class = n_max_per_class
+        self.return_torch_type = return_torch_type
+        self.max_index = max_index
+        self.mean_bgr = mean_bgr
+        self._transform = transform
+        self.semantic_subset = None
+        # TODO(allie): change the line below to allow dif. blob types
+        self.semantic_classes = Defaults.blob_types
+        self.class_names, self.idxs_into_all_blobs = self.get_semantic_names_and_idxs(
+            self.semantic_subset)
+
+        # Blob dynamics
+        self.location_generation_type = Defaults.location_generation_type
+        if self.location_generation_type == 'moving':
+            self.velocity_r_c = Defaults.velocity_r_c if velocity_r_c is None else velocity_r_c
+            if self.n_instances_per_img != 2:
+                assert NotImplementedError('Gotta pick initial conditions (random?)')
+            else:
+                self.initial_rows = initial_rows if initial_rows is not None else \
+                    [0, self.img_size[0] - self.blob_size[0]]
+                self.initial_cols = initial_cols if initial_cols is not None else \
+                    [0, self.img_size[1] - self.blob_size[1]]
+            assert len(self.velocity_r_c) == self.n_instances_per_img, ValueError
+            assert len(self.velocity_r_c[0]) == 2, ValueError
+            max_index_r_c = [max_index if abs(self.velocity_r_c[0][j]) == 0 else
+                             np.floor((self.img_size[j] - self.blob_size[j]) /
+                                      abs(self.velocity_r_c[0][j])) for j in [0, 1]]
+            self.max_index = np.min(max_index_r_c)
+        elif self.location_generation_type == 'random':
+            self.max_index = max_index
+            n_instance_classes = self.n_max_per_class * len(self.semantic_classes)
+            self.random_rows = np.random.randint(0, self.img_size[0] - self.blob_size[
+                0], (max_index + 1, n_instance_classes))
+            self.random_cols = np.random.randint(0, self.img_size[1] - self.blob_size[
+                1], (max_index + 1, n_instance_classes))
+            # TODO(allie): check for overlap and get rid of it.
+
+        else:
+            self.max_index = max_index
+
+    def __getitem__(self, image_index):
+        img, instance_lbl = self.generate_img_lbl_pair(image_index)
+        if self._transform:
+            img, instance_lbl = self.transform(img, instance_lbl)
+        return img, instance_lbl
+
+    def __len__(self):
+        return self.max_index + 1
+
+    def copy(self, modified_length=10):
+        my_copy = BlobExampleGenerator(_im_a_copy=True)
+        for attr, val in self.__dict__.items():
+            setattr(my_copy, attr, val)
+        assert modified_length <= len(my_copy), "Can\'t create a copy with more examples than " \
+                                                "the initial dataset"
+        my_copy.max_index = modified_length - 1
+        return my_copy
+
+    def get_blob_coordinates(self, image_index, instance_idx=None):
+        if self.location_generation_type == 'moving':
+            if instance_idx is not None:
+                r = self.initial_rows[instance_idx] + self.velocity_r_c[instance_idx][
+                                                          0] * image_index
+                c = self.initial_cols[instance_idx] + self.velocity_r_c[instance_idx][
+                                                          1] * image_index
+                return r, c
+            instance_rows = [self.initial_rows[i] + self.velocity_r_c[i][0] * image_index
+                             for i, start_row in enumerate(self.initial_rows)]
+            instance_cols = [start_col + self.velocity_r_c[i][1] * image_index
+                             for i, start_col in enumerate(self.initial_cols)]
+            return instance_rows, instance_cols
+        elif self.location_generation_type == 'random':
+            if instance_idx is not None:
+                r = self.random_rows[image_index, instance_idx]
+                c = self.random_cols[image_index, instance_idx]
+                return r, c
+            instance_rows = [self.random_rows[image_index, i] for i in range(
+                self.n_instances_per_img)]
+            instance_cols = [self.random_cols[image_index, i] for i in range(
+                self.n_instances_per_img)]
+            return instance_rows, instance_cols
+        else:
+            raise ValueError
+
+    def generate_img_lbl_pair(self, image_index):
+        img = np.zeros(self.img_size + (3,), dtype=float)
+        lbl = np.zeros(self.img_size, dtype=int)
+        for semantic_idx, semantic_class in enumerate(self.semantic_classes):
+            for instance_number in range(self.n_instances_per_img):
+                instance_label = semantic_idx * self.n_max_per_class + instance_number + 1
+                r, c = self.get_blob_coordinates(image_index, instance_label)
+                if semantic_class == 'square':
+                    img = self.paint_my_square_in_img(img, r, c, self.clrs[semantic_idx])
+                    lbl = self.paint_my_square_in_lbl(lbl, r, c, instance_label)
+                elif semantic_class == 'circle':
+                    img = self.paint_my_circle_in_img(img, r, c, self.clrs[semantic_idx])
+                    lbl = self.paint_my_circle_in_lbl(lbl, r, c, instance_label)
+                else:
+                    ValueError('I don\'t know how to draw {}'.format(semantic_class))
+        if not lbl.max() < self.n_max_per_class * len(self.semantic_classes) + 1:
+            import ipdb;
+            ipdb.set_trace()
+        return img, lbl
+
+    def paint_my_circle_in_img(self, img, r, c, clr):
+        # noinspection PyTypeChecker
+        return paint_circle(img, r, c, self.blob_size[0], self.blob_size[1], clr,
+                            allow_overflow=True, row_col_dims=(0, 1), color_dim=2)
+
+    def paint_my_circle_in_lbl(self, lbl_img, r, c, instance_label):
+        # noinspection PyTypeChecker
+        return paint_circle(lbl_img, r, c, diameter_a=self.blob_size[0],
+                            diameter_b=self.blob_size[0],
+                            clr=instance_label,
+                            allow_overflow=True, row_col_dims=(0, 1), color_dim=None)
+
+    def paint_my_square_in_img(self, img, r, c, clr):
+        return paint_square(img, r, c, self.blob_size[0], self.blob_size[1], clr,
+                            allow_overflow=True, row_col_dims=(0, 1), color_dim=2)
+
+    def paint_my_square_in_lbl(self, lbl_img, r, c, instance_label):
+        # noinspection PyTypeChecker
+        return paint_square(lbl_img, r, c, self.blob_size[0], self.blob_size[0], instance_label,
+                            allow_overflow=True, row_col_dims=(0, 1), color_dim=None)
+
+    def transform(self, img, lbl):
+        img = dataset_utils.transform_img(img, self.mean_bgr)
+        lbl = dataset_utils.transform_lbl(lbl)
+        return img, lbl
+
+    def untransform(self, img, lbl):
+        img = dataset_utils.untransform_img(img, self.mean_bgr)
+        lbl = dataset_utils.untransform_lbl(lbl)
+        return img, lbl
+
+    @staticmethod
+    def get_semantic_names_and_idxs(semantic_subset):
+        return dataset_utils.get_semantic_names_and_idxs(semantic_subset=semantic_subset,
+                                                         full_set=ALL_BLOB_CLASS_NAMES)
+
+
+def paint_square(img, start_r, start_c, w, h, clr, allow_overflow=True, row_col_dims=(0, 1),
+                 color_dim=2):
+    """
+    allow_overflow = False: error if square falls off edge of screen.
+    row_col_dims, color_dim: set to (2, 3), 1 if you're using pytorch defaults, for instance
+    (num_images, channels, rows, cols)
+    Other options: (0,1), 2 -- typical image RGB format
+                    (0,1), None -- grayscale format (for labels, for instance)
+    """
+
+    img_h, img_w = img.shape[0], img.shape[1]
+
+    end_r = start_r + h
+    end_c = start_c + w
+    if not allow_overflow:
+        if end_r > img_h:
+            raise Exception('square overflows image.')
+        if end_c > img_w:
+            raise Exception('square overflows image.')
+    else:
+        end_r = min(end_r, img_h)
+        end_c = min(end_c, img_w)
+
+    # Handle 'flat' images
+    if color_dim is None:
+        assert np.isscalar(clr), ValueError
+        assert row_col_dims == (0, 1), NotImplementedError
+        img[start_r:end_r, start_c:end_c] = clr
+        return img
+    # Handle RGB images
+    if row_col_dims == (0, 1):
+        for ci, c in enumerate(clr):
+            img[start_r:end_r, start_c:end_c, ci] = c
+    elif row_col_dims == (2, 3):
+        for ci, c in enumerate(clr):
+            img[:, ci, start_r:end_r, start_c:end_c] = c
+    else:
+        raise NotImplementedError
+    return img
+
+
+def paint_circle(img, start_r, start_c, diameter_a, diameter_b, clr, allow_overflow=True,
+                 row_col_dims=(0, 1),
+                 color_dim=2):
+    """
+    allow_overflow = False: error if square falls off edge of screen.
+    row_col_dims, color_dim: set to (2, 3), 1 if you're using pytorch defaults, for instance
+    (num_images, channels, rows, cols)
+    Other options: (0,1), 2 -- typical image RGB format
+                    (0,1), None -- grayscale format (for labels, for instance)
+    """
+    img_h, img_w = img.shape[0], img.shape[1]
+    end_r = start_r + diameter_b
+    end_c = start_c + diameter_a
+    if not allow_overflow:
+        if end_r > img_h:
+            raise Exception('square overflows image.')
+        if end_c > img_w:
+            raise Exception('square overflows image.')
+    else:
+        end_r = min(end_r, img_h)
+        end_c = min(end_c, img_w)
+
+    a, b = int(diameter_a / 2), int(diameter_b / 2)
+    # Handle 'flat' images
+    if color_dim is None:
+        assert np.isscalar(clr), ValueError
+        assert row_col_dims == (0, 1), NotImplementedError
+        img_shape = img.shape[:2]
+        bool_ellipse_img = valid_ellipse_locations(
+            img_shape, center_r=start_r + b, center_c=start_c + a, b=b, a=a)
+        img = (1 - bool_ellipse_img) * img + (bool_ellipse_img) * clr
+        return img
+    # Handle RGB images
+    if row_col_dims == (0, 1):
+        img_shape = img.shape[:2]
+        bool_ellipse_img = valid_ellipse_locations(
+            img_shape, center_r=start_r + b, center_c=start_c + a, b=b, a=a)
+        bool_indexing = np.tile(bool_ellipse_img[:, :, np.newaxis], (1, 1, 3))
+        img = (1 - bool_indexing) * img + (bool_indexing) * clr
+
+    elif row_col_dims == (2, 3):
+        raise NotImplementedError
+        img_shape = img.shape[2:4]
+        bool_ellipse_img = valid_ellipse_locations(
+            img_shape, center_r=start_r + b, center_c=start_c + a, b=b, a=a)
+        img[np.tile(bool_ellipse_img[np.newaxis, np.newaxis, :, :], (1, 3, 1, 1))] = \
+            np.array(clr)[np.newaxis, :, np.newaxis, np.newaxis]
+    else:
+        raise NotImplementedError
+    return img
+
+
+def valid_ellipse_locations(img_shape, center_r, center_c, b, a):
+    r = np.arange(img_shape[0])[:, None]
+    c = np.arange(img_shape[1])
+    in_ellipse = ((c - center_c) / a) ** 2 + ((r - center_r) / b) ** 2 <= 1
+    return in_ellipse
+
+
+def draw_ellipse_on_pil_img(img, start_r, start_c, end_r, end_c, clr):
+    draw = ImageDraw.Draw(image)
+    draw.ellipse((start_c, start_r, end_c, end_r), fill=clr,
+                 outline=clr)
+    return image

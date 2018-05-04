89d1acc5b688b0ad094d47a3800fa526b73db6f0
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2018-05-04 15:27:44 -0400

    removed breakpoint

--- scripts/train_instances.py
+++ scripts/train_instances.py
@@ -61,8 +61,7 @@ def main():
         torch.cuda.manual_seed(1337)
 
     dataloaders = script_utils.get_dataloaders(cfg, args.dataset, args.cuda, args.single_image_index)
-    import ipdb; ipdb.set_trace()
-
+    
     synthetic_generator_n_instances_per_semantic_id = 2
     n_instances_per_class = cfg['n_instances_per_class'] or \
                             (1 if cfg['single_instance'] else synthetic_generator_n_instances_per_semantic_id)

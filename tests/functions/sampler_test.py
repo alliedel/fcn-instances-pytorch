c84563b0ef8d8ad6c943ea36039992174c6616e1
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2018-05-06 13:37:22 -0400

    fixing samplers to pass test

--- tests/functions/sampler_test.py
+++ tests/functions/sampler_test.py
@@ -1,15 +1,67 @@
 import os
-import torch
+import torch.utils.data
 import tqdm
-from torchfcn.datasets import dataset_utils
-from torchfcn.datasets import voc
+
+from torchfcn.datasets import samplers
 from torchfcn import script_utils
+
 from scripts.configurations import voc_cfg
+
+
+def test_vanilla_sampler(train_dataset, loader_kwargs):
+    # Get sampler
+    full_sequential_train_sampler = samplers.sampler_factory(sequential=True)(train_dataset)
+    full_random_train_sampler = samplers.sampler_factory(sequential=False)(train_dataset)
+
+    # Apply sampler to dataloaders
+    sequential_train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=False,
+                                                          sampler=full_sequential_train_sampler, **loader_kwargs)
+    shuffled_train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=False,
+                                                        sampler=full_random_train_sampler, **loader_kwargs)
+    for train_loader in [sequential_train_loader, shuffled_train_loader]:
+        for idx, (d, (sem_lbl, inst_lbl)) in tqdm.tqdm(enumerate(train_loader), desc='Iterating through new dataloader',
+                                                       total=len(train_loader)):
+            pass
+        assert len(train_loader) == len(train_dataset)
+
+    # Make sure shuffling/not shuffling is correct
+    for compare_i in range(10):  # don't bother checking the whole dataset for computational reasons.
+        indexed_image_from_sequential_loader = [d for i, (d, ls) in enumerate(sequential_train_loader)
+                                                if i == compare_i][0][0, ...]
+        assert torch.equal(train_dataset[compare_i][0], indexed_image_from_sequential_loader)
+    subset_sequential_train_loader = [d for i, d in enumerate(sequential_train_loader) if i < 10]
+    subset_random_train_loader = [d for i, d in enumerate(shuffled_train_loader) if i < 10]
+    assert not all([torch.equal(data1[0], data2[0]) for (data1, data2) in zip(subset_random_train_loader,
+                                                                              subset_sequential_train_loader)])
+
+
+def test_single_image_sampler(train_dataset, loader_kwargs, image_index=0):
+    # Get sampler
+    bool_index_subset = [idx == image_index for idx in range(len(train_dataset))]
+    single_image_train_sampler = samplers.sampler_factory(sequential=True,
+                                                          bool_index_subset=bool_index_subset)(train_dataset)
+    shuffled_single_image_train_sampler = samplers.sampler_factory(sequential=False,
+                                                                   bool_index_subset=bool_index_subset)(train_dataset)
+
+    # Apply sampler to dataloaders
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=False,
+                                               sampler=single_image_train_sampler, **loader_kwargs)
+
+    for idx, (d, (sem_lbl, inst_lbl)) in tqdm.tqdm(enumerate(train_loader), desc='Iterating through new dataloader',
+                                                   total=len(train_loader)):
+        pass
+    assert len(train_loader) == 1
+    image_from_train_loader = [d for d in train_loader][0][0][0, ...]
+    assert all([torch.equal(data1[0], data2[0][0, ...]) for data1, data2 in zip(train_dataset[image_index],
+                                                                                image_from_train_loader)])
+    assert all([torch.equal(data1[0], data2[0]) for (data1, data2) in zip(single_image_train_sampler,
+                                                                          shuffled_single_image_train_sampler)])
 
 
 def main():
     # Setup
     cfg = voc_cfg.default_config
+    print('Getting datasets')
     train_dataset, val_dataset = script_utils.get_voc_datasets(cfg, '/home/adelgior/data/datasets/')
     gpu = 0
     os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu)
@@ -19,17 +71,10 @@ def main():
         torch.cuda.manual_seed(1337)
 
     loader_kwargs = {'num_workers': 4, 'pin_memory': True} if cuda else {}
-
-    # Get sampler
-    train_sampler = dataset_utils.sampler_factory(sequential=True)(train_dataset)
-
-    # Apply sampler to dataloaders
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=False, sampler=train_sampler,
-                                               **loader_kwargs)
-
-    for idx, (d, (sem_lbl, inst_lbl)) in tqdm.tqdm(enumerate(train_loader), desc='Iterating through new dataloader',
-                                                   total=len(train_loader)):
-        pass
+    print('Running single-image test')
+    test_single_image_sampler(train_dataset, loader_kwargs, image_index=10)
+    print('Running vanilla test')
+    test_vanilla_sampler(train_dataset, loader_kwargs)
 
 
 if __name__ == '__main__':
--- torchfcn/datasets/dataset_utils.py
+++ torchfcn/datasets/dataset_utils.py
@@ -1,90 +1,10 @@
 import torch
 import numpy as np
 import scipy.misc
-from torch.utils.data import sampler
 
 # TODO(allie): Allow for augmentations
 
 DEBUG_ASSERT = True
-
-
-def sampler_factory(sequential, index_weights=None, bool_index_subset=None):
-    """
-    sequential: False -- will shuffle the images.  True -- will return the same order of images for each call
-    weights: weights to assign to each image
-    bool_filter: True for each index you'd like to include in the sampler
-    """
-
-    class SubsetWeightedSampler(sampler.Sampler):
-        def __init__(self, initial_indices):
-            self.initial_indices = initial_indices
-            self.sequential = sequential
-            self.indices = self.get_sample_indices_from_initial(self.initial_indices)
-
-        def __iter__(self):
-            if sequential:
-                return iter(range(len(self.indices)))
-            else:
-                return iter(torch.randperm(len(self.indices)).long())
-
-        @classmethod
-        def get_sample_indices_from_initial(cls, initial_indices):
-            if index_weights is not None:
-                raise NotImplementedError
-            if bool_index_subset is not None:
-                new_indices = [index for index in initial_indices if bool_index_subset[index]]
-            else:
-                new_indices = initial_indices
-            return new_indices
-    return SubsetWeightedSampler
-
-
-# class SubsetRandomSampler(sampler.Sampler):
-#     r"""Samples elements randomly from a given list of indices, without replacement.
-#
-#     Arguments:
-#         indices (list): a list of indices
-#     """
-#
-#     def __init__(self, indices):
-#         self.indices = indices
-#
-#     def __iter__(self):
-#         return (self.indices[i] for i in torch.randperm(len(self.indices)))
-#
-#     def __len__(self):
-#         return len(self.indices)
-#
-#
-#
-# class WeightedRandomSampler(sampler.Sampler):
-#     r"""Samples elements from [0,..,len(weights)-1] with given probabilities (weights).
-#
-#     Arguments:
-#         weights (list)   : a list of weights, not necessary summing up to one
-#         num_samples (int): number of samples to draw
-#         replacement (bool): if ``True``, samples are drawn with replacement.
-#             If not, they are drawn without replacement, which means that when a
-#             sample index is drawn for a row, it cannot be drawn again for that row.
-#     """
-#
-#     def __init__(self, weights, num_samples, replacement=True):
-#         if not isinstance(num_samples, _int_classes) or isinstance(num_samples, bool) or \
-#                 num_samples <= 0:
-#             raise ValueError("num_samples should be a positive integeral "
-#                              "value, but got num_samples={}".format(num_samples))
-#         if not isinstance(replacement, bool):
-#             raise ValueError("replacement should be a boolean value, but got "
-#                              "replacement={}".format(replacement))
-#         self.weights = torch.tensor(weights, dtype=torch.double)
-#         self.num_samples = num_samples
-#         self.replacement = replacement
-#
-#     def __iter__(self):
-#         return iter(torch.multinomial(self.weights, self.num_samples, self.replacement))
-#
-#     def __len__(self):
-#         return self.num_samples
 
 
 def assert_validation_images_arent_in_training_set(train_loader, val_loader):

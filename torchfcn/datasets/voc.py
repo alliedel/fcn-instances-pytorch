bed9e47f8d841933197f06eeef2ad2efcf6df990
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2018-05-07 13:16:57 -0400

    worried that inst-lbl == 0 is existing for non-bground classes...

--- torchfcn/datasets/voc.py
+++ torchfcn/datasets/voc.py
@@ -241,10 +241,10 @@ class VOCClassSegBase(data.Dataset):
             inst_lbl = self.transform_lbl(inst_lbl)
             inst_lbl[sem_lbl == -1] = -1
             inst_lbl[sem_lbl == 0] = 0  # needed for when we map other semantic classes to background.
-            sem_lbl[inst_lbl > self.n_inst_cap_per_class] = -1
             inst_lbl[inst_lbl > self.n_inst_cap_per_class] = -1
             inst_lbl[inst_lbl == 0] = -1
             inst_lbl[sem_lbl == 0] = 0
+            sem_lbl[inst_lbl == -1] = -1
             if self.return_semantic_instance_tuple:
                 lbl = [sem_lbl, inst_lbl]
             else:

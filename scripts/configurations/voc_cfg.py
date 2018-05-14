010ab2408dded2ebda150bbdb2bbfadf765a2bfc
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2018-05-14 09:49:14 -0400

    created no freeze version for comparison

--- scripts/configurations/voc_cfg.py
+++ scripts/configurations/voc_cfg.py
@@ -84,6 +84,13 @@ configurations = {
         max_iteration=100000,
         freeze_vgg=True,
     ),
+    'person_only__nofreeze__many_itr': dict(
+        semantic_subset=['person', 'background'],
+        set_extras_to_void=True,
+        interval_validate=1000,
+        max_iteration=100000,
+        freeze_vgg=False,
+    ),
     'person_only__3_channels_map_to_semantic__freeze_vgg__many_itr': dict(
         semantic_subset=['person', 'background'],
         set_extras_to_void=True,

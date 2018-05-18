37a86956aa6b357707266a7a4299529610beffca
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2018-05-18 11:10:46 -0400

    in the middle of augmenting with semantic masks

--- torchfcn/datasets/voc.py
+++ torchfcn/datasets/voc.py
@@ -102,7 +102,6 @@ class VOCClassSegBase(data.Dataset):
         self.return_semantic_instance_tuple = return_semantic_instance_tuple
         self.semantic_only_labels = semantic_only_labels
         self.n_inst_cap_per_class = None
-        self.augment_semantic_masks = augment_semantic_masks
 
         # VOC2011 and others are subset of VOC2012
         year = 2012

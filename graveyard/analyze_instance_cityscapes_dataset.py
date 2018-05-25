5c267f2d4b1302bd1e6f383705c0bb96888a9c7b
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2018-05-25 15:07:34 -0400

    added old cityscapes code to graveyard to build off of

---
+++ graveyard/analyze_instance_cityscapes_dataset.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import os.path as osp
+
+import numpy as np
+import torch
+import tqdm
+
+import torchfcn
+from examples.script_utils import get_log_dir
+from torchfcn.datasets import cityscapes
+from torchfcn.datasets import dataset_utils
+
+configurations = {
+    # same configuration as original work
+    # https://github.com/shelhamer/fcn.berkeleyvision.org
+    1: dict(
+        max_iteration=100000,
+        lr=1.0e-10,
+        momentum=0.99,
+        weight_decay=0.0005,
+        interval_validate=4000,
+    )
+}
+
+here = osp.dirname(osp.abspath(__file__))
+
+
+def main():
+    n_max_per_class = 100
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--gpu', type=int, required=True)
+    parser.add_argument('-c', '--config', type=int, default=1,
+                        choices=configurations.keys())
+    parser.add_argument('--resume', help='Checkpoint path')
+    args = parser.parse_args()
+
+    gpu = args.gpu
+    cfg = configurations[args.config]
+    out = get_log_dir(osp.basename(__file__).replace(
+        '.py', ''), args.config, cfg, parent_directory=osp.dirname(osp.abspath(__file__)))
+    print('logdir: {}'.format(out))
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu)
+    cuda = torch.cuda.is_available()
+
+    torch.manual_seed(1337)
+    if cuda:
+        torch.cuda.manual_seed(1337)
+
+    # 1. dataset
+    semantic_subset = None  # ['background', 'person']
+    root = osp.expanduser('~/data/cityscapes/')
+    kwargs = {'num_workers': 4, 'pin_memory': True} if cuda else {}
+    train_dataset = torchfcn.datasets.CityscapesClassSegBase(root, split='train', transform=True,
+                                                      semantic_subset=semantic_subset,
+                                                      n_max_per_class=n_max_per_class,
+                                                      permute_instance_order=False,
+                                                      set_extras_to_void=True,
+                                                      return_semantic_instance_tuple=True)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=False)
+
+    # Make sure we can load an image
+    try:
+        [img, lbl] = train_loader.dataset[0]
+    except:
+        raise
+
+    val_dataset = torchfcn.datasets.CityscapesClassSegBase(root, split='val', transform=True,
+                                                    semantic_subset=semantic_subset,
+                                                    n_max_per_class=n_max_per_class,
+                                                    permute_instance_order=False,
+                                                    set_extras_to_void=True,
+                                                    return_semantic_instance_tuple=True)
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=1, shuffle=False, **kwargs)
+
+    try:
+        [img, lbl] = val_loader.dataset[0]
+    except:
+        raise
+    val_dataset_summary, val_key = create_dataset_summary(val_dataset, n_max_per_class=None)
+    np.savez('/tmp/val_dataset_summary.npz', **val_dataset_summary)
+    np.savez('/tmp/val_key.npz', **val_key)
+    train_dataset_summary, train_key = create_dataset_summary(train_dataset, n_max_per_class=None)
+    np.savez('/tmp/train_dataset_summary.npz', **train_dataset_summary)
+    np.savez('/tmp/train_key.npz', **train_key)
+
+
+def get_n_max_per_class_from_dataset(dataset):
+    n_max_per_class = 0
+    max_sem_idx = 0
+    for idx, (data, (sem_target, inst_target)) in tqdm.tqdm(
+            enumerate(dataset), total=len(dataset),
+            desc='Finding max instances in dataset...', ncols=80,
+            leave=False):
+        n_max_per_class = max(n_max_per_class, torch.max(inst_target)) + 1
+        max_sem_idx = max(max_sem_idx, torch.max(sem_target))
+    assert max_sem_idx == len(cityscapes.ALL_CITYSCAPES_CLASS_NAMES) - 1, 'The dataset contains fewer / more ' \
+                                                            'than the semantic classes I thought'
+    print('\n Max instances: {}'.format(n_max_per_class))
+    return n_max_per_class
+
+
+def create_dataset_summary(dataset, n_max_per_class=None):
+    # Get # max instances
+    if n_max_per_class is None:
+        n_max_per_class = get_n_max_per_class_from_dataset(dataset)
+    dataset.update_n_max_per_class(n_max_per_class)
+
+    # Get some stats
+    semantic_classes = cityscapes.ALL_CITYSCAPES_CLASS_NAMES
+    semantic_instance_mapping = dataset.get_instance_to_semantic_mapping()
+    per_instance_semantic_names = dataset.get_instance_semantic_labels()
+    n_imgs = len(dataset)
+    # Initialize scalars
+    number_of_pixels = np.empty((n_imgs,))
+    number_of_void_pixels = np.empty((n_imgs,))
+    number_of_total_instances = np.empty((n_imgs,))
+    number_of_pixels_per_semantic = np.empty((n_imgs, len(semantic_classes)))
+    number_of_pixels_per_instance_num = np.empty((n_imgs, n_max_per_class))
+    number_of_pixels_per_semantic_instance = np.empty((n_imgs, len(per_instance_semantic_names)))
+    number_of_instances_per_semantic_class = np.empty((n_imgs, len(per_instance_semantic_names)))
+    image_names = []
+    # Get counts for each image
+    for idx, (data, (sem_target, inst_target)) in tqdm.tqdm(
+            enumerate(dataset), total=len(dataset),
+            desc='Analyzing dataset...', ncols=80,
+            leave=False):
+        image_names.append(osp.basename(dataset.files[dataset.split][idx]['img']))
+        number_of_pixels[idx] = torch.numel(sem_target)
+        number_of_pixels_per_semantic[idx, :] = [torch.sum(sem_target == sem_cls)
+                                                 for sem_cls, _ in enumerate(semantic_classes)]
+        number_of_pixels_per_instance_num[idx, :] = [torch.sum(inst_target == inst_num)
+                                                     for inst_num in range(n_max_per_class)]
+        number_of_instances_per_semantic_class[idx, :] = \
+            [torch.max(inst_target[sem_target == sem_cls]) + 1
+             if (torch.sum(sem_target == sem_cls) > 0) else 0
+             for sem_cls, _ in enumerate(semantic_classes)]
+        number_of_total_instances[idx] = np.sum(number_of_instances_per_semantic_class[idx, :])
+        number_of_void_pixels[idx] = torch.sum(sem_target == -1)
+        # The function below overwrites the inst_target, so gotta be careful!
+        full_instance_target = dataset_utils.combine_semantic_and_instance_labels(sem_target,
+                                                                                  inst_target,
+                                                                                  n_max_per_class)
+        number_of_pixels_per_semantic_instance[idx, :] = [torch.sum(full_instance_target ==
+                                                                    sem_inst_cls)
+                                                          for sem_inst_cls in
+                                                          range(len(per_instance_semantic_names))]
+
+    summary = {'image_names': image_names,
+               'number_of_pixels': number_of_pixels,
+               'number_of_pixels_per_semantic': number_of_pixels_per_semantic,
+               'number_of_pixels_per_instance_num': number_of_pixels_per_instance_num,
+               'number_of_pixels_per_semantic_instance': number_of_pixels_per_semantic_instance,
+               'number_of_instances_per_semantic_class': number_of_instances_per_semantic_class,
+               'number_of_void_pixels': number_of_void_pixels,
+               'number_of_total_instances': number_of_total_instances
+               }
+    key = {
+        'semantic_names': semantic_classes,
+        'per_instance_semantic_names': per_instance_semantic_names,
+        'semantic_instance_mapping': semantic_instance_mapping
+    }
+
+    return summary, key
+
+
+if __name__ == '__main__':
+    main()
---
+++ graveyard/cityscapes.py
@@ -0,0 +1,207 @@
+import labels_table_cityscapes
+from torchfcn.datasets import cityscapes_raw
+import numpy as np
+
+
+class CityscapesMappedToInstances(cityscapes_raw.CityscapesWithTransformations):
+    def __init__(self, root, split='train', void_value=-1, resize=True, resize_size=(512, 1024),
+                 cutoff_instances_above=None, make_all_instance_classes_semantic=False):
+        """
+        cutoff_instances_above: int, max number of instances.
+        Note inst_lbl == 0 indicates non-instance label ('rest of them').  We can handle this
+        accordingly after we load the labels.
+        """
+        super(CityscapesMappedToInstances, self).__init__(root, split=split, resize=resize,
+                                                          resize_size=resize_size)
+        self.void_value = void_value
+        self.background_value = 0
+        self.cutoff_instances_above = cutoff_instances_above
+        self.make_all_instance_classes_semantic = make_all_instance_classes_semantic
+
+        # Get dictionary of raw id assignments (semantic, instance, void, background)
+        self._raw_id_list, self._raw_id_assignments = get_raw_id_assignments()
+        # Override if asked to
+        if self.make_all_instance_classes_semantic:
+            self._raw_id_assignments['instance'] = [False for _ in
+                                                    self._raw_id_assignments['instance']]
+
+        # Set list of corresponding training ids, and get training id assignments
+        self._raw_id_to_train_id, self.train_id_list, self.train_id_assignments = \
+            get_train_id_assignments(self._raw_id_assignments, void_value, background_value=0)
+
+        # Get dictionary of class names for each type (semantic, void, background, instance)
+        self.class_names = []
+        for train_id in self.train_id_list:
+            raw_ids_mapped_to_this = [raw_id for raw_id in self._raw_id_list
+                                      if self._raw_id_to_train_id[raw_id] == train_id]
+            class_name = ','.join([labels_table_cityscapes.class_names[i] for i in
+                                   raw_ids_mapped_to_this])
+            self.class_names.append(class_name)
+
+    def __getitem__(self, index):
+        # TODO(allie): Possibly change this convention:
+        """
+        Returns sem_lbl, inst_lbl where:
+        inst_lbl >= 1 for instance classes if instances have been identified
+                    --> we remap these to inst_lbl - 1, so they start from index 0.
+        inst_lbl == 0 for semantic classes
+        inst_lbl == -1 for instance classes that have not been identified
+        Note for instance classes, the 'extras' are -1, but for semantic classes,
+            the 'extras' (all stuff) are 0.  This ensures we can ignore the instance leftovers
+            while still penalizing the semantic classes, without having to carry around
+            identifiers of whether a class is semantic or instance.
+        Note -- when the number of instances for an instance class is just 1, we will
+        break convention and convert it to a semantic class instead.
+        """
+        img, (sem_lbl, inst_lbl) = super(CityscapesMappedToInstances, self).__getitem__(index)
+        sem_lbl = map_raw_sem_ids_to_train_ids(sem_lbl, self._raw_id_list, self._raw_id_to_train_id)
+        inst_lbl = self.raw_inst_to_train_inst_labels(inst_lbl, sem_lbl)
+        return img, (sem_lbl, inst_lbl)
+
+    def modify_length(self, modified_length):
+        self.files[self.split] = self.files[self.split][:modified_length]
+
+    def copy(self, modified_length=10):
+        my_copy = CityscapesMappedToInstances(root=self.root)
+        for attr, val in self.__dict__.items():
+            setattr(my_copy, attr, val)
+        assert modified_length <= len(my_copy), "Can\'t create a copy with more examples than " \
+                                                "the initial dataset"
+        self.modify_length(modified_length)
+        return my_copy
+
+    def raw_inst_to_train_inst_labels(self, inst_lbl, sem_lbl):
+        inst_lbl = map_raw_inst_labels_to_instance_count(inst_lbl)
+        for (sem_train_id, is_instance, is_semantic) in \
+                zip(self.train_id_list, self.train_id_assignments['instance'],
+                    self.train_id_assignments['semantic']):
+            if not is_instance and is_semantic:
+                inst_lbl[sem_lbl == sem_train_id] = 0
+            elif not is_semantic:
+                inst_lbl[sem_lbl == sem_train_id] = -1
+            else:
+                # all instance labels at this point should be in the range (1, n_instances + 1)
+                # remap to (0, n_instances).
+                # Note instance labels given the label 0 will now be void (we don't use
+                # the loss from the leftovers in this implementation, though that will likely
+                #  have to change)
+                # NOTE(allie): removed the line below, because I think we want instance labels
+                # between 1 and (n_instances + 1)
+                # inst_lbl[sem_lbl == sem_train_id] -= 1
+                pass
+        if self.cutoff_instances_above is not None:
+            # Assign 0 to any instances that are not in the range (1, cutoff + 1) -- leftovers
+            inst_lbl[inst_lbl > self.cutoff_instances_above] = 0
+
+        return inst_lbl
+
+
+def get_train_id_assignments(raw_id_assignments, void_value, background_value=0):
+    # train ids include void_value, background_value, and range(1, n_semantic_classes + 1)
+    n_raw_classes = len(raw_id_assignments['semantic'])
+    n_semantic_classes = raw_id_assignments['semantic'].count(True)
+    non_bground_semantic_values = range(1, n_semantic_classes + 1)
+    assert void_value not in non_bground_semantic_values, 'void_value should not overlap with ' \
+                                                          'semantic classes'
+    assert background_value == 0, NotImplementedError
+    # Each semantic class get its own value
+    train_ids = [void_value, background_value] + list(range(n_semantic_classes))
+
+    # Map raw ids to unique train ids
+    sem_cls = 0
+    raw_id_to_train_id = []
+    for raw_id in range(n_raw_classes):
+        if raw_id_assignments['background'][raw_id]:
+            raw_id_to_train_id.append(background_value)
+        elif raw_id_assignments['semantic'][raw_id]:
+            sem_cls += 1
+            raw_id_to_train_id.append(sem_cls)
+        elif raw_id_assignments['void'][raw_id]:
+            raw_id_to_train_id.append(void_value)
+        else:
+            raise Exception('raw_id_assignments does not cover all the classes')
+    assert sem_cls == n_semantic_classes - sum(raw_id_assignments['background']), \
+        'Debug assert failed here.'
+
+    is_background, is_void, is_semantic, is_instance = [], [] , [], []
+    for tid in train_ids:
+        # background
+        is_background.append(tid == background_value)
+        # void
+        is_void.append(tid == void_value)
+        my_raw_ids = [ci for ci, mapped_train_id in enumerate(raw_id_to_train_id)
+                      if mapped_train_id == tid]
+        # semantic
+        is_semantic_for_all_raw = [raw_id_assignments['semantic'][id] for id in my_raw_ids]
+        if not all(is_semantic_for_all_raw[0] == is_sem for is_sem in is_semantic_for_all_raw):
+            raise Exception('Mapped semantic, non-semantic raw classes to the same train id.')
+        is_semantic.append(is_semantic_for_all_raw[0])
+        # instance
+        is_instance_for_all_raw = [raw_id_assignments['instance'][id] for id in my_raw_ids]
+        if not all(is_instance_for_all_raw[0] == is_inst for is_inst in is_instance_for_all_raw):
+            raise Exception('Mapped instance, non-instance raw classes to the same train id')
+        is_instance.append(is_instance_for_all_raw[0])
+
+    train_assignments = {'background': is_background,
+                         'void': is_void,
+                         'semantic': is_semantic,
+                         'instance': is_instance}
+    return raw_id_to_train_id, train_ids, train_assignments
+
+
+def get_raw_id_assignments():
+    """
+    # semantic: All classes we are considering (non-void)
+    # instance: Subset of semantic classes with instances
+    # void: Any classes that don't get evaluated on (too few, or they're unlabeled, etc.)
+    # background: Any classes that get mapped into one big 'miscellanious' class (differs from
+      void because we evaluate on it)
+    """
+    raw_id_list = labels_table_cityscapes.ids
+    is_background = [name == 'unlabeled'
+                     for c, name in enumerate(labels_table_cityscapes.class_names)]
+    is_semantic = [not is_void or is_background[ci] for ci, is_void in enumerate(
+        labels_table_cityscapes.is_void)]
+    has_instances = [has_instances and not is_void for ci, (has_instances, is_void)
+                     in enumerate(zip(labels_table_cityscapes.has_instances,
+                                      labels_table_cityscapes.is_void))]
+    is_void = [is_void and not is_background[ci] for ci, is_void in enumerate(
+        labels_table_cityscapes.is_void)]
+    assert len(is_void) == len(is_background) == len(is_semantic) == len(has_instances)
+    assert all([(b and s) == b for b, s in zip(is_background, is_semantic)])
+    return raw_id_list, {'semantic': is_semantic,
+                         'instance': has_instances,
+                         'void': is_void,
+                         'background': is_background}
+
+
+def map_raw_inst_labels_to_instance_count(inst_lbl):
+    """
+    Specifically for Cityscapes.
+    Warning: inst_lbl must be an int/long for this to work
+    """
+    inst_lbl[inst_lbl < 1000] = 0
+    inst_lbl -= (inst_lbl / 1000) * 1000  # more efficient mod(inst_lbl, 1000)
+    return inst_lbl
+
+
+def map_raw_sem_ids_to_train_ids(sem_lbl, old_values, new_values_from_old_values):
+    """
+    Specifically for Cityscapes. There are a bunch of classes that didn't get used,
+    so they 'remap' them onto actual training classes.  Leads to very silly remapping after
+    loading...
+
+    WARNING: We map iteratively (less memory-intensive than copying sem_lbl or making masks).
+    For this to work, train_ids must be <= ids, background_value <= background_ids,
+    void_value <= void_ids.
+    """
+    new_value_sorted_idxs = np.argsort(new_values_from_old_values)
+    old_values = [old_values[i] for i in new_value_sorted_idxs]
+    new_values_from_old_values = [new_values_from_old_values[i] for i in new_value_sorted_idxs]
+    assert len(old_values) == len(new_values_from_old_values)
+    assert all([new <= old for old, new in zip(old_values, new_values_from_old_values)]), \
+        NotImplementedError('I\'ve got to do something smarter when assigning...')
+    # map ids to train_ids (e.g. - tunnel (id=16) is unused, so maps to 255.
+    for old_val, new_val in zip(old_values, new_values_from_old_values):
+        sem_lbl[sem_lbl == old_val] = new_val
+    return sem_lbl

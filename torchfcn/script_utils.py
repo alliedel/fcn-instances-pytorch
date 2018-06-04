2a5e9a91c8c032e3b8bac9072123d4231973674e
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2018-06-04 11:51:08 -0400

    fixing checkpoint loading

--- torchfcn/script_utils.py
+++ torchfcn/script_utils.py
@@ -316,17 +316,18 @@ def get_problem_config(class_names, n_instances_per_class, map_to_semantic=False
     return problem_config
 
 
-def get_model(cfg, problem_config, checkpoint, semantic_init, cuda):
+def get_model(cfg, problem_config, checkpoint_file, semantic_init, cuda):
     n_input_channels = 3 if not cfg['augment_semantic'] else 3 + problem_config.n_semantic_classes
     model = torchfcn.models.FCN8sInstance(
         semantic_instance_class_list=problem_config.model_semantic_instance_class_list,
         map_to_semantic=problem_config.map_to_semantic, include_instance_channel0=False,
         bottleneck_channel_capacity=cfg['bottleneck_channel_capacity'], score_multiplier_init=cfg['score_multiplier'],
         n_input_channels=n_input_channels, clip=cfg['clip'], add_conv8=cfg['add_conv8'])
-    if checkpoint is not None:
+    if checkpoint_file is not None:
+        checkpoint = torch.load(checkpoint_file)
         model.load_state_dict(checkpoint['model_state_dict'])
-        start_epoch = checkpoint['epoch']
-        start_iteration = checkpoint['iteration']
+        start_epoch = checkpoint_file['epoch']
+        start_iteration = checkpoint_file['iteration']
     else:
         start_epoch, start_iteration = 0, 0
         if cfg['initialize_from_semantic']:
@@ -338,8 +339,8 @@ def get_model(cfg, problem_config, checkpoint, semantic_init, cuda):
                 semantic_instance_class_list=[1 for _ in range(problem_config.n_semantic_classes)],
                 map_to_semantic=False, include_instance_channel0=False)
             print('Copying params from preinitialized semantic model')
-            checkpoint = torch.load(semantic_init_path)
-            semantic_model.load_state_dict(checkpoint['model_state_dict'])
+            checkpoint_file = torch.load(semantic_init_path)
+            semantic_model.load_state_dict(checkpoint_file['model_state_dict'])
             model.copy_params_from_semantic_equivalent_of_me(semantic_model)
         else:
             print('Copying params from vgg16')

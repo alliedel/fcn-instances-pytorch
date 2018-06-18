4e3f408e72d75bbfa72e80cecbf2cf4ecbb32519
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2018-06-18 12:50:11 -0400

    fixed figure size changes

--- scripts/analysis/script_for_location_heatmaps.py
+++ scripts/analysis/script_for_location_heatmaps.py
@@ -10,6 +10,10 @@ from torchfcn import script_utils
 from torchfcn.analysis import score_heatmaps
 
 
+FIGSIZE = (10, 10)
+DPI = 300
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--logdir', help='directory that contains the checkpoint and config', type=str, required=True)
@@ -28,13 +32,14 @@ def write_absolute_heatmaps(absolute_heatmap_average, split, instance_problem):
     sem_inst_class_list = instance_problem.semantic_instance_class_list
     inst_id_list = instance_problem.instance_count_id_list
     channel_names = instance_problem.get_channel_labels()
-
+    plt.figure(0, figsize=FIGSIZE)
+    plt.clf()
     for channel_idx, (sem_idx, inst_id) in enumerate(zip(sem_inst_class_list, inst_id_list)):
         channel_name = channel_names[channel_idx]
         display_pyutils.matshow_and_save_list_to_workspace(
             [absolute_heatmap_average[channel_idx, :, :]],
             filename_base_ext='{}_score_heatmaps_{}.png'.format(split, channel_name),
-            show_filenames_as_titles=True, cmap=COLORMAP)
+            show_filenames_as_titles=True, cmap=COLORMAP, dpi=DPI)
 
 
 def write_relative_heatmaps_by_channel(list_of_relative_heatmap_averages, instance_problem, split):
@@ -46,7 +51,8 @@ def write_relative_heatmaps_by_channel(list_of_relative_heatmap_averages, instan
     ch_subplot_rc_arrangement = [(sem_val, inst_id) for sem_val, inst_id in zip(sem_inst_class_list, inst_id_list)]
     print('Writing heatmaps relative to each channel')
     for channel_idx in range(n_channels):
-        h = plt.figure(0)
+        h = plt.figure(0, figsize=FIGSIZE)
+        plt.clf()
         hm = list_of_relative_heatmap_averages[channel_idx]
         list_of_subtitles = ['rel-to {}'.format(rel_ch_idx, channel_names[rel_ch_idx])
                              for rel_ch_idx in range(n_channels)]
@@ -58,7 +64,7 @@ def write_relative_heatmaps_by_channel(list_of_relative_heatmap_averages, instan
         filename = '{}_score_heatmaps_rel_by_ch_idx_{}.png'.format(
             split, channel_names[channel_idx])
         plt.suptitle(filename)
-        display_pyutils.save_fig_to_workspace(filename)
+        display_pyutils.save_fig_to_workspace(filename, dpi=DPI)
         h.clear()
 
 
@@ -70,7 +76,8 @@ def write_relative_heatmaps_by_sem_cls(list_of_relative_heatmap_averages_rel_by_
     n_channels = len(list_of_relative_heatmap_averages_rel_by_semantic)
     n_semantic_classes = len(sem_class_names)
     for channel_idx in range(n_channels):
-        h = plt.figure(0, figsize=(10, 10), dpi=300)
+        h = plt.figure(0, figsize=FIGSIZE)
+        plt.clf()
         hm = list_of_relative_heatmap_averages_rel_by_semantic[channel_idx]
         list_of_subtitles = ['rel-to {}'.format(sem_class_names[rel_sem_idx]) for rel_sem_idx in range(
             n_semantic_classes)]
@@ -82,7 +89,7 @@ def write_relative_heatmaps_by_sem_cls(list_of_relative_heatmap_averages_rel_by_
             split, channel_names[channel_idx])
         plt.suptitle(filename)
         print('Writing image {}/{}'.format(channel_idx + 1, n_channels))
-        display_pyutils.save_fig_to_workspace(filename)
+        display_pyutils.save_fig_to_workspace(filename, dpi=DPI)
         h.clear()
 
 

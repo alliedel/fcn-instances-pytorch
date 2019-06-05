ca89e90b5249daf8218f36e79d58f978ce671d4d
allie <adelgior@cs.cmu.edu>
2019-06-05 16:10:17 -0400

    removed unused model

--- instanceseg/models/__init__.py
+++ instanceseg/models/__init__.py
@@ -2,5 +2,4 @@
 from .fcn8s_instance import FCN8sInstance, FCN8sInstancePretrained
 from .resnet_instance import ResNet50Instance
 from .resnet import Resnet, ResNetBackbone
-from .simple_sym_fcn import SimpleSymmetricFCN
 from .vgg import VGG16
